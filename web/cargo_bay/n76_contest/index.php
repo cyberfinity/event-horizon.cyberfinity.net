6edb8f429ffcd2e4e0be8d747814d0ae6692c646
James Nash <cirrus@twiddles.com>
2023-10-31 11:40:42 +0000

    removed broken links

--- web/cargo_bay/n76_contest/index.php
+++ web/cargo_bay/n76_contest/index.php
@@ -4,19 +4,19 @@
 	require('./header.inc');
 ?>
 		<div id="main">
-			<p>This mini-site showcases the N76 phone designs <a href="http://yvette.twiddles.com/">Ha Yun Park</a> made for Nokia's <a href="http://en.wikipedia.org/wiki/Nokia_Nseries">N-Series</a> &ldquo;Your Talent&rdquo; contest:</p>
+			<p>This mini-site showcases the N76 phone designs <a href="https://cirrus.twiddles.com/">James Nash</a> made for Nokia's <a href="http://en.wikipedia.org/wiki/Nokia_Nseries">N-Series</a> &ldquo;Your Talent&rdquo; contest:</p>
 			<ul id="designlinks">
 				<li><a href="dirtydoodles/"><img src="dd.jpg" alt="[Preview of Dirty Doodles design]" />Dirty Doodles</a></li>
 				<li><a href="skull/"><img src="sk.jpg" alt="[Preview of Skull design]" />Skull</a></li>
 				<li><a href="midnightdoodles/"><img src="md.jpg" alt="[Preview of Midnight Doodles design]" />Midnight Doodles</a></li>
 			</ul>
 			<p class="clear">Explore each design in more detail via the links above!</p>
-			<p>Each design was created from scratch by <a href="http://yvette.twiddles.com/">Ha Yun Park</a> using a combination tradional and digital art. The individual design pages above explain the thinking and process behind each design. We hope you enjoy them. If you do, then don't forget to tell your friends!</p>
+			<p>Each design was created from scratch by <a href="https://cirrus.twiddles.com/">James Nash</a> using a combination tradional and digital art. The individual design pages above explain the thinking and process behind each design. We hope you enjoy them. If you do, then don't forget to tell your friends!</p>
 
 			<h2>About the contest</h2>
 			<p>The object of the <a href="http://en.wikipedia.org/wiki/Nokia_Nseries">N-Series</a> &ldquo;Your Talent&rdquo; contest was to design a cover for Nokia's N76 mobile phone. Submitted designs appeared on the competition website and could be rated by visitors. When the contest finished in August 2007, the creators of the top 10 most popular designs each won an N76 mobile phone with their design printed on.</p>
-			<p>Before appearing on the competition website, designs were subject to a manual review by the organisers to ensure that they complied with the rules (i.e. no copyright violations or offensive content). The approval or rejection of a design by the reviewers was supposed to happen within a "few days" but in Ha Yun's case took much longer. Her <a href="dirtydoodles/">Dirty Doodles</a> design was approved on 11. August - <em>the last day of the contest</em> - and the other designs were neither approved nor rejected at that point. Oddly, those designs were eventually approved and put on the site on 24. August, the day <em>after</em> the winners were announced!</p>
-			<p>With barely a day to promote her design and for visitors to vote for it, <a href="dirtydoodles/">Dirty Doodles</a> did not achieve a good rank, let alone the top 10. If it had been approved and appeared on the competition website earlier it may well have had better chances.</p>
+			<p>Before appearing on the competition website, designs were subject to a manual review by the organisers to ensure that they complied with the rules (i.e. no copyright violations or offensive content). The approval or rejection of a design by the reviewers was supposed to happen within a "few days" but in James's case took much longer. His <a href="dirtydoodles/">Dirty Doodles</a> design was approved on 11. August - <em>the last day of the contest</em> - and the other designs were neither approved nor rejected at that point. Oddly, those designs were eventually approved and put on the site on 24. August, the day <em>after</em> the winners were announced!</p>
+			<p>With barely a day to promote his design and for visitors to vote for it, <a href="dirtydoodles/">Dirty Doodles</a> did not achieve a good rank, let alone the top 10. If it had been approved and appeared on the competition website earlier it may well have had better chances.</p>
 
 			<h2>Friendly designs</h2>
 			<p>The friendly folk over at the <a href="http://www.symbian-freak.com/forum/viewtopic.php?p=134537">Symbian-Freak forums</a> also produced some nice N76 designs for the contest:</p>
@@ -30,8 +30,7 @@
 		<div id="side">
 			<div class="box" id="first">
 				<h2>About the designer</h2>
-				<img src="yve_logo.jpg" alt="Ha Yun's Avatar" />
-				<p>Ha Yun (also known as Yvette) likes dabbling with art and design in her spare time. Visit her website: <a href="http://yvette.twiddles.com/">Yvette's World</a>!</p>
+				<p>James likes dabbling with art and design in his spare time. Visit his website: <a href="https://cirrus.twiddles.com/">Cirrus's Realm</a>!</p>
 			</div>
 		</div>
 <?php
