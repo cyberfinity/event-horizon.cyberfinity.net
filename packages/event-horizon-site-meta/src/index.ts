eb8a42424673da6dfdd5167aa7b41dc6ab411360
James Nash <cirrus@twiddles.com>
2025-07-13 01:14:56 +0100

    fix. maybe?

--- packages/event-horizon-site-meta/src/index.ts
+++ packages/event-horizon-site-meta/src/index.ts
@@ -1,13 +1,19 @@
-import { env } from "node:process";
+import process from "node:process";
 import { join } from "node:path";
 import { fileURLToPath } from "node:url";
 
 const siteDistRoot = fileURLToPath(new URL("../../../dist/", import.meta.url));
 
-export const site =
-  env["CONTEXT"] === "deploy-preview"
-    ? env["DEPLOY_PRIME_URL"]
-    : "https://event-horizon.cyberfinity.net";
+export function site(): string {
+  const siteUrl =
+    process.env["CONTEXT"] === "deploy-preview"
+      ? (process.env["DEPLOY_PRIME_URL"] as string)
+      : "https://event-horizon.cyberfinity.net";
+
+  console.log(`site() is: ${siteUrl}`);
+
+  return siteUrl;
+}
 
 function makeCargoBayPath(siteDir: string): string {
   return `/cargo_bay/${siteDir}/`;
--- sites/bcp-outposts/astro.config.js
+++ sites/bcp-outposts/astro.config.js
@@ -2,11 +2,9 @@
 import { defineConfig } from "astro/config";
 import { site, bcpOutposts } from "@cyberfinity/event-horizon-site-meta";
 
-console.log("Site is: ", site);
-
 // https://astro.build/config
 export default defineConfig({
-  site,
+  site: site(),
   base: bcpOutposts.path,
   outDir: bcpOutposts.distDir,
 });
