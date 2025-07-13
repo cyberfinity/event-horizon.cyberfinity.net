06ea75b1cbb1d1510e9a7c37d80f0ceab05d1745
James Nash <cirrus@twiddles.com>
2025-07-13 01:06:25 +0100

    debug

--- sites/bcp-outposts/astro.config.js
+++ sites/bcp-outposts/astro.config.js
@@ -2,6 +2,8 @@
 import { defineConfig } from "astro/config";
 import { site, bcpOutposts } from "@cyberfinity/event-horizon-site-meta";
 
+console.log("Site is: ", site);
+
 // https://astro.build/config
 export default defineConfig({
   site,
